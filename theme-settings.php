86045f9bc5efc11d85cb50ed2392a4b214910090
Ramesh Rasaiyan <rasaiyan.ramesh@gmail.com>
2013-05-21 21:27:27 -0400

    Footer has been implemented

---
+++ theme-settings.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Implements hook_form_FORM_ID_alter().
+ *
+ * @param $form
+ *   The form.
+ * @param $form_state
+ *   The form state.
+ */
+function azhagu_form_system_theme_settings_alter(&$form, &$form_state) {
+
+  $form['azhagu_settings']['socialblock'] = array(
+    '#type' => 'fieldset',
+    '#title' => t('Social Icon'),
+    '#collapsible' => TRUE,
+    '#collapsed' => FALSE,
+  );
+  $form['azhagu_settings']['socialblock']['socialblock_display'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Show Social Icon'),
+    '#default_value' => theme_get_setting('socialblock_display','azhagu'),
+    '#description'   => t("Check this option to show Social Icon. Uncheck to hide."),
+  );
+  $form['azhagu_settings']['socialblock']['twitter_url'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Twitter Profile URL'),
+    '#default_value' => theme_get_setting('twitter_url', 'azhagu'),
+    '#description'   => t("Enter your Twitter Profile URL. Leave blank to hide."),
+  );
+  $form['azhagu_settings']['socialblock']['facebook_url'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Facebook Profile URL'),
+    '#default_value' => theme_get_setting('facebook_url', 'azhagu'),
+    '#description'   => t("Enter your Facebook Profile URL. Leave blank to hide."),
+  );
+  $form['azhagu_settings']['socialblock']['gplus_url'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Google Plus Address'),
+    '#default_value' => theme_get_setting('gplus_url', 'azhagu'),
+    '#description'   => t("Enter your Google Plus URL. Leave blank to hide."),
+  );
+  $form['azhagu_settings']['socialblock']['pinterest_url'] = array(
+    '#type' => 'textfield',
+    '#title' => t('Pinterest Address'),
+    '#default_value' => theme_get_setting('pinterest_url', 'azhagu'),
+    '#description'   => t("Enter your Pinterest URL. Leave blank to hide."),
+  );
+}
